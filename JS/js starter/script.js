895a95a4c2ea25ed2d22cc23579eb8b489a0f5ed
hknshn55 <hknshn55@hotmail.com>
2025-08-06 15:13:31 +0300

    js baslangic

---
+++ JS/js starter/script.js
@@ -0,0 +1,13 @@
+//    // alert("Baslangic");
+//     let message = "Merhaba";
+//     // alert(message)
+//     console.log(50+5);
+
+let message // undefined
+console.log(message);// undefined
+message = "hello"
+console.log(message);//Hello
+message = "Merhaba"
+console.log(message);//Merhaba
+
+

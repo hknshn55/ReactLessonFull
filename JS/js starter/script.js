7fb54e80cc72b4375fe1a22a2e2055a46e01f9d0
hknshn55 <hknshn55@hotmail.com>
2025-08-06 15:30:08 +0300

    not eklendi

--- JS/js starter/script.js
+++ JS/js starter/script.js
@@ -3,11 +3,27 @@
 //     // alert(message)
 //     console.log(50+5);
 
-let message // undefined
-console.log(message);// undefined
-message = "hello"
-console.log(message);//Hello
-message = "Merhaba"
-console.log(message);//Merhaba
+// let message // undefined
+// console.log(message);// undefined
+// message = "hello"
+// console.log(message);//Hello
+// message = "Merhaba"
+// console.log(message);//Merhaba
+
+
+let greeting = "Merhaba";
+let message;
+message = greeting; 
+console.log(message);
+
+//userName
+//user_name
+//3year degiskenler sayi ile baslayamaz.(ozel karakter _,$ harici baslangicta kullanilamaz ozel kelimeleri kullanamaz)
+
+// Hata ile karsilasilan yerde durur ve oradan sonrasini calistirmaz.
+
+//  let person = "Hakan"
+//  let Person = "Hakan" kucuk buyuk harf duyarliligi vardir.
+
 
 

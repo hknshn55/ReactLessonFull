f04309468b3b9ca1496accfa95d0150f1e061e31
hknshn55 <hknshn55@hotmail.com>
2025-08-07 15:45:15 +0300

    yeni ornek

--- JS/example3/index.js
+++ JS/example3/index.js
@@ -29,3 +29,4 @@ console.log(typeof lastName);
 
 
 // < > >= <= || && ==     + - * / %     += /= *= -=  ++  --
+
---
+++ JS/example4/index.js
@@ -0,0 +1,54 @@
+/* ==== Challenge 1: ====
+  - name adında bir değişken oluşturun
+  - Değerini adınıza ve soyadınıza atayın 
+  - Bu değişkenin sabit olması gerekiyor mu? 
+    Eğer bu bilgiyi hiç değiştirmeyecekseniz, const kullanmanız daha doğru olur.
+    Ama kullanıcıdan gelen bir bilgi olacaksa let tercih edebilirsiniz.
+*/
+
+// name için kod buraya yazılır
+const name ="Hakan Şahin";
+
+/* ==== Challenge 2: ====
+  - age adında bir değişken oluşturun
+  - Yaş zamanla değişebilen bir bilgi olduğu için genellikle let ile tanımlanır.
+  - Ama sabit bir yaş üzerinden işlem yapacaksanız const da kullanabilirsiniz.
+*/
+
+// age için kod buraya yazılır
+let age = 18;
+
+/* ==== Challenge 3: ====
+  - myLocation adında bir değişken oluşturun
+  - Değeri şu an bulunduğunuz yer olmalı ( şehir veya ülke)
+  - Konumunuz değişebilir mi? Eğer evet diyorsanız let, hayır diyorsanız const kullanabilirsiniz.
+*/
+
+// myLocation için kod buraya yazılır
+let myLocation = "Kocaeli"
+
+/* ==== Challenge 4: ====
+  - favoriteBook adında bir değişken oluşturun 
+  - En sevdiğiniz kitap zamanla değişebilir.
+    Karar sizin: Kalıcı bir favori ise const, değişebilir diyorsanız let kullanabilirsiniz.
+*/
+
+// favoriteBook için kod buraya yazılır
+let favoriteBook = "Bir Gun Herşey Çok Güzel Olacak"
+
+/* ==== Challenge 5: ====
+  - favoriteBand adında bir değişken oluşturun 
+  - Zamanla müzik zevkiniz değişebilir mi? Bu soruya göre const ya da let kullanabilirsiniz.
+*/
+
+// favoriteBand için kod buraya yazılır
+let favoriteBand = "Pop"
+
+/* ==== Challenge 6: ====
+  - favoriteFood adında bir değişken oluşturun 
+  - Bu bilgi de kişisel tercihe bağlı olarak zamanla değişebilir.
+    Sabit mi, dönemsel mi? Hangisi uygunsa ona göre değişken tipi belirleyin.
+*/
+
+// favoriteFood için kod buraya yazılır
+let favoriteFood = "Makarna"

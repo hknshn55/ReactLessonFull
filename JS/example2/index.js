3756819fbbdcdfddbb728ae367ed9a604706801b
hknshn55 <hknshn55@hotmail.com>
2025-08-07 12:56:47 +0300

    yeni konu tipler

---
+++ JS/example2/index.js
@@ -0,0 +1,16 @@
+// let currentUser;
+// let activeUser;
+// let newUser;
+// let newMan;
+
+// let obj= {
+//    name:"Hakan"
+// }
+
+// Primitive/ilkel
+let userName = "Hakan"
+let age = 28
+// Infinity,-Infinity,NaN---->Not a Number
+
+console.log(); // Kısayol : clg + tab
+console.log(1/0); // Infinity

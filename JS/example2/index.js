36de991d50a9f3fdd2e304ae2f5fbc5d9152bb1c
hknshn55 <hknshn55@hotmail.com>
2025-08-07 13:40:48 +0300

    yeni konu tipler

--- JS/example2/index.js
+++ JS/example2/index.js
@@ -3,14 +3,26 @@
 // let newUser;
 // let newMan;
 
+const { use } = require("react")
+
 // let obj= {
 //    name:"Hakan"
 // }
 
 // Primitive/ilkel
-let userName = "Hakan"
-let age = 28
+// let userName = "Hakan"
+// let age = 28
 // Infinity,-Infinity,NaN---->Not a Number
 
-console.log(); // Kısayol : clg + tab
-console.log(1/0); // Infinity
+// console.log(); // Kısayol : clg + tab
+// console.log(1/0); // Infinity
+// console.log(Infinity);
+
+
+// $ bu sembol ile string ifadenin icerisine disaridan deger getirilebilir yani gomulebilir.
+
+// let userName = "Hakan"
+// let userName2 = "Hakan" 
+// let userName3 = `Deger gomulebilir ${userName}`
+
+// Boolean

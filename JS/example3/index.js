3b9f58c9026fb1a0bf127edd64e94f64a5aa04a2
hknshn55 <hknshn55@hotmail.com>
2025-08-07 14:39:21 +0300

    yeni ornek

---
+++ JS/example3/index.js
@@ -0,0 +1,30 @@
+// === Veri Tipleri ve Değişken Tanımlama Üzerine Alıştırma ===
+
+const { use } = require("react");
+
+// 1. "isEven" adında bir değişken oluşturun ve içine boolean bir değer yazın.
+//    Yaşınızın 26 olduğunu varsayalım. Çift olduğu için bu değişkene true yazabilirsiniz.
+//    (Hesaplama yapmanıza gerek yok, doğrudan true yazmanız yeterli)
+let isEven = true;
+
+//    Ayrıca "lastName" adında bir değişken oluşturun ama içerisine henüz bir değer atamayın.
+//    (Yani sadece tanımlayın, boş bırakın. Böylece tipi undefined olur)
+let lastName;
+
+// 2. "city" adında bir değişken oluşturun ve yaşadığınız şehrin adını atayın.
+//    (Şehir ismini tırnak içinde yazmalısınız çünkü string değer olacak)
+let city = "Kocaeli";
+
+// 3. Şimdi sırasıyla şu değişkenlerin veri türlerini konsola yazdırın:
+//    - isEven
+//    - user (öncesinde bu değişkeni de tanımlamayı unutmayın)
+//    - city
+//    - lastName
+let user = "hakan"
+console.log(typeof isEven);
+console.log(typeof user);
+console.log(typeof city);
+console.log(typeof lastName);
+
+//    (Türünü görmek için typeof ifadesini kullanabilirsiniz)
+//    console.log(typeof isEven)
